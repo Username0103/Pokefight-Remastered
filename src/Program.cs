62f2517519514484e71012c23332569079aa5bca
Username0103 <49798750+Username0103@users.noreply.github.com>
2025-07-15 23:51:21 -0300

    added some testing stuff to Program.cs

--- src/Program.cs
+++ src/Program.cs
@@ -1,15 +1,43 @@
 ﻿using System.Text.Json;
-using src.data;
+using Src.Data;
+using Src.Misc;
 
-namespace src
+namespace Src
 {
     public class Program
     {
+        private static readonly JsonSerializerOptions options = new()
+        {
+            IncludeFields = true,
+            WriteIndented = true,
+            IndentSize = 2,
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        };
+
         public static void Main(string[] args)
         {
+            var player = new AudioPlayer();
+            player.Play(Song.TitleScreen);
+            Console.ReadKey();
+        }
+
+        private static void PrintAllPokemon()
+        {
             Database.Initialize();
-            var options = new JsonSerializerOptions { IncludeFields = true, WriteIndented = true };
-            Console.WriteLine(JsonSerializer.Serialize(Methods.GetAllPokemon()[149], options));
+            var pokemon = Methods.GetAllPokemon(out var effectivenesses);
+            Console.WriteLine($"Found: {string.Join(", ", pokemon.Select((p) => p.Name))}.");
+        }
+
+        private static void PrintAllData()
+        {
+            Database.Initialize();
+            Console.WriteLine("{\nPokemon\": ");
+            Console.Write(
+                JsonSerializer.Serialize(Methods.GetAllPokemon(out var effectivenesses), options)
+            );
+            Console.WriteLine(",\n\t\"Effectivenesses\":");
+            Console.WriteLine(JsonSerializer.Serialize(effectivenesses, options));
+            Console.Write("}");
         }
     }
 }
