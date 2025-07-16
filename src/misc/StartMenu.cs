f13438c1d887e6c74e63d3e5db0a7ab34e109d20
Username0103 <49798750+Username0103@users.noreply.github.com>
2025-07-15 23:49:21 -0300

    added start of UI static class StartMenu

---
+++ src/misc/StartMenu.cs
@@ -0,0 +1,12 @@
+using Spectre.Console;
+
+namespace Src.Misc
+{
+    public static class StartMenu
+    {
+        public static void SelectPokemon()
+        {
+            AnsiConsole.Markup("[bold white]Welcome to the world of Pokemon![/] ");
+        }
+    }
+}
