534780dbfa9e0bdfdf0ec38f63d6fd633bd09b21
Username0103 <49798750+Username0103@users.noreply.github.com>
2025-07-21 22:45:51 -0300

    added required column to PokemonMoves DB model class

--- src/data/models/PokemonMoves.cs
+++ src/data/models/PokemonMoves.cs
@@ -8,6 +8,7 @@ namespace Src.Data.models
         public int pokemon_id { get; set; }
         public int move_id { get; set; }
         public int level { get; set; }
+        public int version_group_id { get; set; }
 
         [Column("pokemon_move_method_id")]
         public int learning_method { get; set; }
