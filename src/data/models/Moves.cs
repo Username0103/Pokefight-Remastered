2c8b4387982987ff85842a145da5c1e11beb324e
Username0103 <49798750+Username0103@users.noreply.github.com>
2025-07-21 22:46:36 -0300

    added required column to Moves db model class

--- src/data/models/Moves.cs
+++ src/data/models/Moves.cs
@@ -5,11 +5,13 @@ namespace Src.Data.models
     [Table("moves")]
     public class Moves
     {
+        public string identifier { get; set; } = null!;
+
         [Column("id")]
         public int move_id { get; set; }
         public int type_id { get; set; }
-        public string identifier { get; set; } = null!;
         public int pp { get; set; }
+        public int generation_id { get; set; }
         public int damage_class_id { get; set; }
         public int priority { get; set; }
         public int? accuracy { get; set; }
